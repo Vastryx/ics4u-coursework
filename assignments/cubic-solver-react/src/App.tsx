52dd609713e3ae0298ed1ffaf71ccd09645a2e49
Vastryx <180879565+Vastryx@users.noreply.github.com>
2026-03-11 10:48:02 -0400

    1

--- assignments/cubic-solver-react/src/App.tsx
+++ assignments/cubic-solver-react/src/App.tsx
@@ -7,12 +7,12 @@ import type { CoefficientsType } from './types';
 export function App() {
 	const [coefficients, setcoefficients] = useState<CoefficientsType>();
 
-	console.log(coefficients);
+	function handleSubmit(name: string, value: number) {}
 
 	return (
 		<div className="m-auto max-w-4/5 p-8">
 			<div className="rounded-2xl border border-gray-300 p-6">
-				<CubicInput set={setcoefficients} />
+				<CubicInput handler={handleSubmit} />
 			</div>
 			<div className="mt-4 rounded-2xl border border-gray-300 p-6">
 				<CubicEquation />
--- assignments/cubic-solver-react/src/components/CubicInput.tsx
+++ assignments/cubic-solver-react/src/components/CubicInput.tsx
@@ -1,24 +1,12 @@
-import type { CoefficientsType } from '../types';
-
 type CubicInputProps = {
-	set: React.Dispatch<React.SetStateAction<CoefficientsType | undefined>>;
+	handler: (name: string, value: number) => void;
 };
 
-export function CubicInput({ set }: CubicInputProps) {
-	function handleSubmit(formData: FormData) {
-		const [a, b, c, d] = formData.values().map(Number);
-		set({
-			a,
-			b,
-			c,
-			d,
-		});
-	}
-
+export function CubicInput({ handler }: CubicInputProps) {
 	return (
 		<>
 			<h1 className="text-4xl font-semibold">Cubic Solver</h1>
-			<form className="flex gap-4" action={handleSubmit}>
+			<form className="flex gap-4">
 				<div className="flex shrink grow basis-3xs flex-col gap-1.5">
 					<label htmlFor="a">a value:</label>
 					<input className="w-full rounded-lg border px-3" type="number" name="a" required />
@@ -33,7 +21,7 @@ export function CubicInput({ set }: CubicInputProps) {
 				</div>
 				<div className="flex shrink grow basis-3xs flex-col gap-1.5">
 					<label htmlFor="d">d value:</label>
-					<input className="w-full rounded-lg border px-3" type="number" required />
+					<input className="w-full rounded-lg border px-3" type="number" name="d" required />
 				</div>
 				<input
 					className="flex w-auto shrink-0 grow-0 cursor-pointer rounded-lg border border-gray-900 px-5 font-semibold"
